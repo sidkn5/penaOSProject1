58ee462e53aedeba192c58f04d69f5ed857428f0
Sean Pena <pena@hoare7.cs.umsl.edu>
2021-02-08 15:10:22 -0600

    third commit

--- doenv.c
+++ doenv.c
@@ -33,34 +33,46 @@ void printEnv2(){
 		printf("%s=%s\n", j, getenv(j));
 	}
 }
-
-
-int iOption(int argc, char **argv){
+//no option given, updates the environment
+void noOptionUpdate(int argc, char **argv){
 	int noOfChars = 0;
 	int i = 0;
+}
+
+//ignores the current environment and replaces it with the new one
+void iOption(int argc, char **argv){
+	int noOfChars = 0;
+	int i = 0;
+
+	//find shell command
+
 	for (i=2; i < argc; i++){
 		char *str1 = argv[i];
-		char *utility = "utility";
-		int ret = strcmp(str1, utility);
-		if (ret == 0){
-			printf("found utility\n");
-			//do something when utility is used
-		} else {
+		int x;
+		int sizeOfStr1 = strlen(argv[i]);
+		char buffer[1];
+		strcpy(buffer, str1);
+		char *ptr;
+		int ch = '=';
+
+		ptr = strchr(buffer, ch);
+		if ( ptr == NULL){
+			printf("i'm a system call\n");
+			system(argv[i]);
+			
+		}else {
 			noOfChars++;
 		}
 	}
-
 	
 	char **newEnvironment = malloc(sizeof(char *) * (noOfChars + 1));
 
-	char *arg = optarg;
 
 	if(optind < argc){
 		for(i=0;i < noOfChars; i++){
 			int size = strlen(argv[i]);
-			char *currentArg = argv[i+2];
 			newEnvironment[i] = (char*)malloc(sizeof(char *) * (size + 1));			
-			newEnvironment[i] = argv[optind];
+			newEnvironment[i] = argv[optind-1];
 			optind++;
 			
 		}
@@ -75,11 +87,35 @@ int iOption(int argc, char **argv){
 		printf("An error occured.");
 	}
 	
+	for (i=2; i < argc; i++){
+		char *str1 = argv[i];
+		int x;
+		int sizeOfStr1 = strlen(argv[i]);
+		char buffer[1];
+		strcpy(buffer, str1);
+		char *ptr;
+		int ch = '=';
 
+		ptr = strchr(buffer, ch);
+		if ( ptr == NULL){
+			printf("i'm a system call\n");
+			system(argv[i]);	
+		}
+	}
 }
 
 //not done
 void help(){
+	printf("Help: \n");
+	printf("Usage: ./doenv [option] [name1=val1] ... [namen=valn] command1 ... commandn \n");
+	printf("This program behaves in the same way as the env utility when executing another program. \n");
+	printf("Example: ./doenv -i TZ=EST date\n");
+	printf("Options: \n");
+	printf("-h \t displays help information on how this program is to be called or executed\n");
+	printf("-i \t ignores the current environment and replaces it with the name=value pair entered by the user.\n");
+	printf("A call without arguments or an option will simply print the current evironment. \n");
+	printf("A call without an option but with name=value arguments modifies/updates the current environment. \n");
+
 }
 
 int main(int argc, char *argv[], char *envp[]){
@@ -91,7 +127,7 @@ int main(int argc, char *argv[], char *envp[]){
 	}
 
 	//
-	while ((c = getopt(argc, argv,"ih:")) != -1){
+	while ((c = getopt(argc, argv,"hi:")) != -1){
 		switch (c){
 			//-i option used
 			case 'i':
@@ -101,7 +137,11 @@ int main(int argc, char *argv[], char *envp[]){
 			case 'h':
 			help();
 			break;
+
+			default:
+			printEnv(envp);
 		}
 	}
+
 	return 0;
 }
